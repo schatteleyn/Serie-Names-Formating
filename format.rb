a14be2b12736c501967de59a26a6b05a25a92478
Simon <kilik2049@gmail.com>
2011-05-27 14:02:11 +0200

    ask serie's name, correct wrong request

--- format.rb
+++ format.rb
@@ -1,17 +1,20 @@
+puts "Nom de la série ?"
+serie = gets.chomp
 puts "Nombre d'épisodes ?"
 episode = gets.chomp
 puts "Numéro de la saison?"
 saison = gets
-puts "Il y a #{episode} épisodes dans la saison #{saison}"
+puts "Format:#{serie} S#{saison}E#{episode}"
 
 i = 1
 directory = Dir.pwd
 d = Dir.new(directory)
+
 d.each do |file|
   until (i <= episode)
     old_name = File.basename(__FILE__)   #get filename
-    suffix = File.extname(file_name)     #get file extension
-    File.rename(old_name, "S#{saison}E#{i}.#{suffix}")
+    suffix = File.extname(__FILE__)     #get file extension
+    File.rename(old_name, "#{serie} S#{saison}E#{i}.#{suffix}")
     i += 1
   end
 end
