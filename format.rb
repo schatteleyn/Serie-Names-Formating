96775636338b9bf79386c51ed1d60e344e971628
Simon <kilik2049@gmail.com>
2011-05-28 19:56:24 +0200

    add S0*

--- format.rb
+++ format.rb
@@ -1,20 +1,29 @@
-puts "Nom de la série ?"
+puts "Serie's name ?"
 serie = gets.chomp
-puts "Nombre d'épisodes ?"
+puts "Episode's number ?"
 episode = gets.chomp
-puts "Numéro de la saison?"
-saison = gets
-puts "Format:#{serie} S#{saison}E#{episode}"
+puts "Season's number?"
+season = gets
+puts "Format:#{serie} S#{season}E#{episode}"
 
 i = 1
 directory = Dir.pwd
 d = Dir.new(directory)
 
-d.each do |file|
-  until (i <= episode)
-    old_name = File.basename(__FILE__)   #get filename
-    suffix = File.extname(__FILE__)     #get file extension
-    File.rename(old_name, "#{serie} S#{saison}E#{i}.#{suffix}")
-    i += 1
-  end
+if d == nil
+	puts "Wrong path"
+else
+	d.each do |file|
+	  until (i <= episode)
+	    old_name = File.basename(__FILE__)   #get filename
+	    suffix = File.extname(__FILE__)     #get file extension
+      if i < 10
+        File.rename(old_name, "#{serie} S#{season}E0#{i}.#{suffix}")
+        i += 1
+      else
+        File.rename(old_name, "#{serie} S#{season}E#{i}.#{suffix}")
+        i += 1
+      end
+	  end
+	end
 end
