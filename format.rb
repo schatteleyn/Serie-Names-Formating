521197aa02880447f414d01c4f4e54aba99076a6
Simon <kilik2049@gmail.com>
2011-05-28 23:20:03 +0200

    add season format support

--- format.rb
+++ format.rb
@@ -1,13 +1,14 @@
+puts "Directory ?"
+directory = gets.chomp
 puts "Serie's name ?"
 serie = gets.chomp
 puts "Episode's number ?"
 episode = gets.chomp
 puts "Season's number?"
 season = gets
-puts "Format:#{serie} S#{season}E#{episode}"
+puts "Format: #{serie} S#{season}E#{episode}"
 
 i = 1
-directory = Dir.pwd
 d = Dir.new(directory)
 
 if d == nil
@@ -17,13 +18,19 @@ else
 	  until (i <= episode)
 	    old_name = File.basename(__FILE__)   #get filename
 	    suffix = File.extname(__FILE__)     #get file extension
-      if i < 10
-        File.rename(old_name, "#{serie} S#{season}E0#{i}.#{suffix}")
-        i += 1
-      else
-        File.rename(old_name, "#{serie} S#{season}E#{i}.#{suffix}")
-        i += 1
-      end
-	  end
-	end
+        if i < 10 and season < 10
+          File.rename(old_name, "#{serie} S0#{season}E0#{i}.#{suffix}")
+          i += 1
+        elsif i < 10 and season > 10
+          File.rename(old_name, "#{serie} S#{season}E0#{i}.#{suffix}")
+          i += 1
+        elsif i > 10 and season < 10
+          File.rename(old_name, "#{serie} S0#{season}E#{i}.#{suffix}")
+          i += 1
+        else
+          File.rename(old_name, "#{serie} S#{season}E#{i}.#{suffix}")
+          i += 1
+        end
+    end
+  end
 end
