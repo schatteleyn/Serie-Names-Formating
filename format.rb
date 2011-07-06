c3df5bfe0a7531fe60a6411d21d2ddfa43f3ce7e
Simon <kilik2049@gmail.com>
2011-07-07 00:28:25 +0200

    sanitize the loop and make it functionnal

--- format.rb
+++ format.rb
@@ -1,14 +1,13 @@
 puts "Directory ?"
-directory = gets.chomp
+  directory = gets.chomp
 puts "Serie's name ?"
-serie = gets.chomp
+  serie = gets.chomp
 puts "Total episode's number ?"
-episode = gets.chomp
+  episode = gets.chomp
 puts "Season's number?"
-season = gets.chomp
+  season = gets.chomp
 puts "Format: #{serie} S#{season}E#{episode}"
 
-i = 1
 d = Dir.new(directory)
 Dir.chdir(directory)
 
@@ -16,22 +15,13 @@ if d == nil
 	puts "Wrong path"
 else
 	d.each do |file|
-	  until (i <= episode)
+	  for episode in (1..episode.to_i)
+	 	episode = "%02d" % episode
+	 	season = "%02d" % season
 	    old_name = File.basename(__FILE__)   #get filename
 	    suffix = File.extname(__FILE__)      #get file extension
-        if i < 10 and season < 10			 #manage format with season et episode's number
-          File.rename(old_name, "#{serie} S0#{season}E0#{i}.#{suffix}")
-          i += 1
-        elsif i < 10 and season > 10
-          File.rename(old_name, "#{serie} S#{season}E0#{i}.#{suffix}")
-          i += 1
-        elsif i > 10 and season < 10
-          File.rename(old_name, "#{serie} S0#{season}E#{i}.#{suffix}")
-          i += 1
-        else
-          File.rename(old_name, "#{serie} S#{season}E#{i}.#{suffix}")
-          i += 1
-        end
+      #manage format with season et episode's number
+      File.rename(old_name, "#{serie} S#{season}E#{episode}.#{suffix}")
     end
   end
 end
