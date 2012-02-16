ce191e64326b2f7c92b7f68e36baf5482629ae3c
Simon <kilik2049@gmail.com>
2012-02-16 15:58:45 -0500

    Improve the script. Don't need to provide season or episodes' numbers

--- format.rb
+++ format.rb
@@ -2,10 +2,6 @@ puts "Directory ?"
   directory = gets.chomp
 puts "Serie's name ?"
   serie = gets.chomp
-puts "Total episode's number ?"
-  episode = gets.chomp
-puts "Season's number?"
-  season = gets.chomp
 puts "Format: #{serie} S#{season}E#{episode}"
 
 d = Dir.new(directory)
@@ -15,13 +11,11 @@ if d == nil
 	puts "Wrong path"
 else
 	d.each do |file|
-	  for episode in (1..episode.to_i)
-	 	episode = "%02d" % episode
-	 	season = "%02d" % season
-	    old_name = File.basename(__FILE__)   #get filename
-	    suffix = File.extname(__FILE__)      #get file extension
-      #manage format with season et episode's number
-      File.rename(old_name, "#{serie} S#{season}E#{episode}.#{suffix}")
-    end
+		old_name = File.basename(__FILE__)
+		suffix = File.extname(__FILE__)
+		episode = old_name.match(/[eE]\d+[\-]?\d+/).to_s.match(/\d+[\-]?\d+/).to_s
+		season = old_name.match([sS]\d+).to_s.match(\d+).to_s
+		#Must cover more possibilities with regex
+		File.rename(old_name, "#{serie} S#{season}E#{episode}.#{suffix}")
   end
 end
