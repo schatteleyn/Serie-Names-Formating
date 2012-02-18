2cf3f63f9564c11feb760bc0ad15f56ec258c592
Simon Chatteleyn <kilik2049@gmail.com>
2012-02-17 23:38:07 -0500

    Format the episode and season's number - via github

--- format.rb
+++ format.rb
@@ -14,7 +14,9 @@ else
 		old_name = File.basename(__FILE__)
 		suffix = File.extname(__FILE__)
 		episode = old_name.match(/[eE]\d+[\-]?\d+/).to_s.match(/\d+[\-]?\d+/).to_s
+		episode = "%02d" % episode #Format the number
 		season = old_name.match([sS]\d+).to_s.match(\d+).to_s
+		season = "%02d" % season #Format
 		#Must cover more possibilities with regex
 		File.rename(old_name, "#{serie} S#{season}E#{episode}.#{suffix}")
   end
