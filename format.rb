065945bf0647d5c7501b0b7c1da1fa4b8407462f
Simon Chatteleyn <kilik2049@gmail.com>
2012-02-22 14:20:27 -0500

    redo indentation

--- format.rb
+++ format.rb
@@ -8,16 +8,16 @@ d = Dir.new(directory)
 Dir.chdir(directory)
 
 if d == nil
-	puts "Wrong path"
+  puts "Wrong path"
 else
-	d.each do |file|
-		old_name = File.basename(__FILE__)
-		suffix = File.extname(__FILE__)
-		episode = old_name.match(/[eE]\d+[\-]?\d+/).to_s.match(/\d+[\-]?\d+/).to_s
-		episode = "%02d" % episode #Format the number
-		season = old_name.match(/[sS]\d+/).to_s.match(/\d+/).to_s
-		season = "%02d" % season #Format
-		#Must cover more possibilities with regex
-		File.rename(old_name, "#{serie} S#{season}E#{episode}.#{suffix}")
+  d.each do |file|
+    old_name = File.basename(__FILE__)
+    suffix = File.extname(__FILE__)
+    episode = old_name.match(/[eE]\d+[\-]?\d+/).to_s.match(/\d+[\-]?\d+/).to_s
+    episode = "%02d" % episode #Format the number
+    season = old_name.match(/[sS]\d+/).to_s.match(/\d+/).to_s
+    season = "%02d" % season #Format
+    #Must cover more possibilities with regex
+    File.rename(old_name, "#{serie} S#{season}E#{episode}.#{suffix}")
   end
 end
