cd6fb84d7e3e8d6f3c32c77d40c372f927f75ca8
Simon Chatteleyn <kilik2049@gmail.com>
2012-05-16 14:56:50 -0400

    fix directories problem, have to fix . & .. case

--- format.rb
+++ format.rb
@@ -6,9 +6,10 @@ puts "Format: #{serie} Sxx Exx"
 
 if Dir.exist?(directory)
   Dir.chdir(directory)
-  d.each do |file|
-    old_name = File.basename(__FILE__)
-    suffix = File.extname(__FILE__)
+  files = Dir.entries(Dir.pwd)
+  files.each do |file|
+    old_name = File.basename(file)
+    suffix = File.extname(file)
     episode = old_name.match(/[eE]\d+[\-]?\d+/).to_s.match(/\d+[\-]?\d+/).to_s
     episode = "%02d" % episode.to_i #Format the number
     season = old_name.match(/[sS]\d+/).to_s.match(/\d+/).to_s
