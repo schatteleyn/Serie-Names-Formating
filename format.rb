f62461c7b8cf762b0f1c6cf2404fce69cb794ca4
Simon Chatteleyn <kilik2049@gmail.com>
2012-05-16 16:51:56 -0400

    add regex to avoid hidden files

--- format.rb
+++ format.rb
@@ -6,17 +6,21 @@ puts "Format: #{serie} Sxx Exx"
 
 if Dir.exist?(directory)
   Dir.chdir(directory)
-  files = Dir.entries(Dir.pwd).drop(2) # Delete "." and ".." directories
+  files = Dir.entries(Dir.pwd) # list all the files
   files.each do |file|
-    old_name = File.basename(file)
-    suffix = File.extname(file)
-    episode = old_name.match(/[eE]\d+[\-]?\d+/).to_s.match(/\d+[\-]?\d+/).to_s
-    episode = "%02d" % episode.to_i #Format the number
-    season = old_name.match(/[sS]\d+/).to_s.match(/\d+/).to_s
-    season = "%02d" % season.to_i #Format
-    #Must cover more possibilities with regex
-    File.rename(old_name, "#{serie} S#{season}E#{episode}#{suffix}")
-    puts "#{serie} S#{season}E#{episode}#{suffix}"
+    if /[.]*.+/.match(file) # Avoid ".", "..", ".DS_Store" and every hidden files
+    	next
+    else
+      old_name = File.basename(file)
+      suffix = File.extname(file)
+      episode = old_name.match(/[eE]\d+[\-]?\d+/).to_s.match(/\d+[\-]?\d+/).to_s
+      episode = "%02d" % episode.to_i #Format the number
+      season = old_name.match(/[sS]\d+/).to_s.match(/\d+/).to_s
+      season = "%02d" % season.to_i #Format
+      #Must cover more possibilities with regex
+      File.rename(old_name, "#{serie} S#{season}E#{episode}#{suffix}")
+      puts "#{serie} S#{season}E#{episode}#{suffix}"
+    end
   end
 else
   puts "Wrong path"
