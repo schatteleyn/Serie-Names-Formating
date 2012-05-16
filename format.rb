4099567a5f2ab790254c725119d60824f1ab0142
Simon Chatteleyn <kilik2049@gmail.com>
2012-05-16 14:08:44 -0400

    extname give the dot, change formatting

--- format.rb
+++ format.rb
@@ -2,7 +2,7 @@ puts "Directory ?"
   directory = gets.chomp
 puts "Serie's name ?"
   serie = gets.chomp
-puts "Format: #{serie} S#{season}E#{episode}"
+puts "Format: #{serie} Sxx Exx"
 
 d = Dir.new(directory)
 Dir.chdir(directory)
@@ -14,10 +14,10 @@ else
     old_name = File.basename(__FILE__)
     suffix = File.extname(__FILE__)
     episode = old_name.match(/[eE]\d+[\-]?\d+/).to_s.match(/\d+[\-]?\d+/).to_s
-    episode = "%02d" % episode #Format the number
+    episode = "%02d" % episode.to_i #Format the number
     season = old_name.match(/[sS]\d+/).to_s.match(/\d+/).to_s
-    season = "%02d" % season #Format
+    season = "%02d" % season.to_i #Format
     #Must cover more possibilities with regex
-    File.rename(old_name, "#{serie} S#{season}E#{episode}.#{suffix}")
+    File.rename(old_name, "#{serie} S#{season}E#{episode}#{suffix}")
   end
 end
