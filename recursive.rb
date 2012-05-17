d990a92afbfb03677f2f00ebd1046248f407dc16
Simon Chatteleyn <kilik2049@gmail.com>
2012-05-17 11:55:50 -0400

    add file to test recursive opening and renomming. Not usable for now. Use only format.rb

---
+++ recursive.rb
@@ -0,0 +1,7 @@
+dir.each do |file|
+  if File.exist?(file) # Si true, le fichier est un répertoire
+    # Ouvrir le répertoire, recommencer
+   else
+     # affichage du chemin, demander nom série, lancer le script
+  end
+end
