8f8da6aa87b6a587d9095ed5a900dbe5d499b039
Simon Chatteleyn <kilik2049@gmail.com>
2012-09-25 14:31:19 +0200

    first implementation. Gotta do more test with ds_store bullshit for exemple

--- recursive.rb
+++ recursive.rb
@@ -1,12 +1,45 @@
 #!/usr/bin/env ruby
-$LOAD_PATH << File.dirname(__FILE__)
-require 'format.rb'
+
 require 'find'
 
 Find.find('./') do |f|
-  if Dir.exist?(f) == false
-    path = Dir.pwd
+  if !Dir.exist?(f)
+    path = Dir.pwd + f[1..-1] # get full path, removing the "."
     format(path)
-    f.prune
+  end
+end
+
+def format(directory)
+  puts directory
+  puts "Serie's name ?"
+    serie = gets.chomp
+  puts "Format: #{serie} Sxx Exx"
+
+  if Dir.exist?(directory)
+    Dir.chdir(directory)
+    files = Dir.entries(Dir.pwd) # list all the files
+    files.each do |file|
+      if /^[.]+.*/.match(file) # Avoid ".", "..", ".DS_Store" and every hidden files
+        next
+      else
+        old_name = File.basename(file)
+        suffix = File.extname(file)
+        if old_name.match(/\d+[x]\d+/)
+          w = old_name.match(/\d+[x]\d+/).to_s
+          season = w.match(/^\d{1,2}/).to_s
+          episode = w.match(/\d{1,2}$/).to_s
+        else
+          episode = old_name.match(/[eE]\d+[\-eE]?\d+/).to_s.match(/\d+[\-eE]?\d+/).to_s
+          season = old_name.match(/[sS]\d+/).to_s.match(/\d+/).to_s
+        end
+        episode = "%02d" % episode.to_i #Format
+        season = "%02d" % season.to_i #Format
+        File.rename(old_name, "#{serie} S#{season}E#{episode}#{suffix}")
+        puts "#{old_name} is now named:"
+        puts "    #{serie} S#{season}E#{episode}#{suffix}"
+      end
+    end
+  else
+    puts "Wrong path"
   end
 end
